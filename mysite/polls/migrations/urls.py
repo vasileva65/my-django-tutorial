9b72d69c8aeb9e5a6d9e48b8a36bfd2a0ba88183
vasileva65 <vasilieva.alena65@gmail.com>
2023-02-14 13:34:14 +0500

    polls

--- mysite/polls/migrations/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.contrib import admin
-
-from . import views
-
-urlpatterns = [
-    path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
-]
